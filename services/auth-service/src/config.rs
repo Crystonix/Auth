8eb98d08acbee0f7d3a6424d18fe7e81b076b0e3
Joshua Jung || jj050 <128308156+Crystonix@users.noreply.github.com>
2026-01-24 08:07:18 +0100

    WORKING

--- services/auth-service/src/config.rs
+++ services/auth-service/src/config.rs
@@ -27,7 +27,6 @@ impl Config {
         let key_bytes: [u8; 32] = key_bytes_vec
             .try_into()
             .expect("Decoded key must be exactly 32 bytes");
-        let app_env = env::var("APP_ENV").unwrap_or_else(|_| "development".to_string());
 
         Self {
             auth_service_port: env::var("AUTH_SERVICE_PORT").expect("AUTH_SERVICE_PORT must be set").parse().expect("AUTH_SERVICE_PORT must be a number"),
@@ -40,7 +39,7 @@ impl Config {
             discord_auth_url: env::var("DISCORD_AUTH_URL").expect("DISCORD_AUTH_URL must be set"),
             discord_token_url: env::var("DISCORD_TOKEN_URL").expect("DISCORD_TOKEN_URL must be set"),
             encryption_key: key_bytes,
-            is_production: app_env == "production",
+            is_production: "production" == env::var("ENVIRONMENT").expect("ENVRONMENT must be set"),
             frontend_url: env::var("FRONTEND_URL").expect("FRONTEND_URL must be set"),
         }
     }
